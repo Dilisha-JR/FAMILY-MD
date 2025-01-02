26d85855c838e1150333d3b7b2fa16c42e65b4e3
Dilisha-JR <dilishagimshan852@gmail.com>
2025-01-02 12:13:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "hK1jmAhb#lp6__zjP10c-XYvNbCmt2waYCrjIsGObuFAJhAybFmU",
+SESSION_ID: process.env.SESSION_ID || "XrJRSaoA#p_nvlK-Mr5zTZAcDC4X9mKYUC-6W1Au7ABXrrALDjRc",
 ALIVE_IMG : process.env.ALIVE_IMG || "https://i.ibb.co/Tk76N76/9752.jpg",
 ALIVE_MSG : process.env.ALIVE_MSG || "*𝐇𝐈 𝐁𝐎𝐓 𝐈𝐒 𝐀𝐋𝐈𝐕𝐄 𝐍𝐎𝐖\n\n𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐃: = ꧁𓊈𒆜 ᗪIᒪIᔕᕼᗩ 𒆜𓊉꧂\n\n𝐓𝐄𝐀𝐌: = ✿.｡.:* ☆:**:. 𝕐𝔸𝕂𝕌ℤ𝔸 .:**:.☆*.:｡.✿\n\nYOUTUBE: https://www.youtube.com/@shanstudio-n1x \n\nWHATSAPP: https://chat.whatsapp.com/Fz9ycW1PBIU5idc37JqMNv \n\n𝐓𝐎: 𝚃𝙷𝙰𝙽𝙺𝚂 𝙵𝙾𝚁 𝚄𝚂𝙸𝙽𝙶 𝙵𝙰𝙼𝙸𝙻𝚈-𝙼𝙳*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
